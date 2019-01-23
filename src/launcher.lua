ec79fa72db8161fc192632b37f7bb535b8741e20
David Briscoe <idbrii@gmail.com>
2019-01-23 12:41:32 -0800

    Fix indentation

--- src/launcher.lua
+++ src/launcher.lua
@@ -5,21 +5,21 @@ local Launcher = class('Launcher')
 Launcher.collision_class = 'Building'
 
 function Launcher:initialize(gamestate, x, y)
-        table.insert(gamestate.entities, self)
-        print("Launcher:", "creating at", x, y)
-        love.window.setTitle(string.format("Launcher: creating at %i,%i", x, y), 10, 10)
-        self.radius = 10
-        self.collider = gamestate.world:newCircleCollider(x, y, self.radius)
-        self.collider:setRestitution(0.8)
-        self.collider:setCollisionClass(Launcher.collision_class)
-        self.collider:applyLinearImpulse(500, 500)
+    table.insert(gamestate.entities, self)
+    print("Launcher:", "creating at", x, y)
+    love.window.setTitle(string.format("Launcher: creating at %i,%i", x, y), 10, 10)
+    self.radius = 10
+    self.collider = gamestate.world:newCircleCollider(x, y, self.radius)
+    self.collider:setRestitution(0.8)
+    self.collider:setCollisionClass(Launcher.collision_class)
+    self.collider:applyLinearImpulse(500, 500)
 end
 
 function Launcher:update()
 end
 function Launcher:draw()
-        local cx,cy = self.collider:getPosition()
-        love.graphics.circle('fill', cx, cy, self.radius)
+    local cx,cy = self.collider:getPosition()
+    love.graphics.circle('fill', cx, cy, self.radius)
 end
 
 return Launcher
