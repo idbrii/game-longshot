5b9e9080518191e7963830c7a9a0470b5280fd1e
David Briscoe <idbrii@gmail.com>
2019-01-23 15:17:15 -0800

    Give app a title

---
+++ conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+    t.identity     = 'longshot' -- The name of the save directory (string)
+    t.version      = '11.2'     -- The LÖVE version this game was made for (string)
+    t.console      = true       -- Attach a console (boolean, Windows only)
+    t.window.title = 'Longshot' -- The window title (string)
+    --~ t.window.x = 700                    -- The x-coordinate of the window's position in the specified display (number)
+    --~ t.window.y = nil                    -- The y-coordinate of the window's position in the specified display (number)
+    --~ t.window.width = 1920                -- The window width (number)
+    --~ t.window.height = 1280               -- The window height (number)
+end
