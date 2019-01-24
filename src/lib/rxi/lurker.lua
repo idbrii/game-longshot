9561ce96d9d4896df4f97cc2e4f1f933edfffe52
David Briscoe <idbrii@gmail.com>
2019-01-24 11:19:28 -0800

    Grab PR to fix lurker
    But it doesn't fix it. Similar fix to what I already applied.
    
    Lurker does seem to hotload (has output), but nothing changes.

--- src/lib/rxi/lurker.lua
+++ src/lib/rxi/lurker.lua
@@ -14,20 +14,13 @@ local lume = rawget(_G, "lume") or require((...):gsub("[^/.\\]+$", "lume"))
 local lurker = { _version = "1.0.1" }
 
 
+local major, minor, revision, codename = love.getVersion()
 local dir = love.filesystem.enumerate or love.filesystem.getDirectoryItems
-local isdir = love.filesystem.isDirectory
 local time = love.timer.getTime or os.time
-local lastmodified = love.filesystem.getLastModified
-if love.filesystem.getInfo then
-  isdir = function(path)
-    local info = love.filesystem.getInfo(path, "directory")
-    return info ~= nil
-  end
-  lastmodified = function(path)
-    local info = love.filesystem.getInfo(path, "directory")
-    return info and info.modtime or nil
-  end
-end
+local isdir = (major < 11) and love.filesystem.isDirectory or
+    function(item) return love.filesystem.getInfo(item).type == "directory" end
+local lastmodified = (major < 11) and love.filesystem.getLastModified or
+    function(item) return love.filesystem.getInfo(item).modtime end
 
 local lovecallbacknames = {
   "update",
