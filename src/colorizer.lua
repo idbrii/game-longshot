c4fab37620a830d2b292af0fbcce2ed687298dc4
David Briscoe <idbrii@gmail.com>
2019-01-25 14:39:25 -0800

    Color tweaks and new art from phil

--- src/colorizer.lua
+++ src/colorizer.lua
@@ -4,9 +4,9 @@ function colorizer.getColour(index)
     -- values are R G B
     -- 1 for 100%, 0.5 for 50%
     if index == 1 then
-        return .33, .33, 1
+        return .5, .33, 1
     else
-        return 1, .33, .33
+        return 1, .33, .5
     end
 end
 
