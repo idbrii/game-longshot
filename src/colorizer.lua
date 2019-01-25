1f189243b483cf96f9e0a435009145eeefac37c1
David Briscoe <idbrii@gmail.com>
2019-01-24 16:48:22 -0800

    Comment

--- src/colorizer.lua
+++ src/colorizer.lua
@@ -1,10 +1,12 @@
 local colorizer = {}
 
 function colorizer.getColour(index)
+    -- values are R G B
+    -- 1 for 100%, 0.5 for 50%
     if index == 1 then
-        return 0, 255, 0
+        return 0, 1, 0
     else
-        return 255, 0, 0
+        return 1, 0, 0
     end
 end
 
