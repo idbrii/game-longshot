25892abf4c792d0d3375949ca9becc730f90424c
David Briscoe <idbrii@gmail.com>
2019-01-25 01:00:10 -0800

    Increase idle life
    because I was getting deaths on spawn

--- src/tuning.lua
+++ src/tuning.lua
@@ -16,7 +16,8 @@ local tuning = {
     timer = {
         projectile = {
             max_lifetime = 10,
-            max_idle_lifetime = 0.5,
+            -- can't be too small or might die during initial spawn and fall
+            max_idle_lifetime = 0.85,
         },
     },
 }
